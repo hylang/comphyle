c1b51723960b1b590fb7bd4105c63b3d17166633
Paul R. Tagliamonte <paultag@debian.org>
2013-04-13 01:03:00 -0400

    Add in a version thinger.

--- comphyle/native.py
+++ comphyle/native.py
@@ -1,6 +1,8 @@
 #XXX: We need to move shit out of here.
 
 from contextlib import contextmanager
+from sh import dpkg
+import sh
 
 
 @contextmanager
@@ -28,3 +30,46 @@ def tmpwork():
     with tmpdir() as tmp_path:
         with cd(tmp_path):
             yield tmp_path
+
+
+
+class V(object):
+
+    _opers = ["lt", "le", "eq", "ne", "ge", "gt"]
+
+    def __init__(self, version_string):
+        self.version_string = version_string
+
+    def _cmp(self, oper, other):
+        if not isinstance(other, V):
+            raise TypeError("Stop sucking.")
+        if oper not in self._opers:
+            raise ValueError("Blow it out 'yer ass")
+
+        try:
+            dpkg("--compare-versions", self.version_string,
+                 oper, other.version_string)
+            return True
+        except sh.ErrorReturnCode_1:
+            return False
+
+    def __lt__(self, other):
+        return self._cmp("lt", other)
+
+    def __le__(self, other):
+        return self._cmp("lt", other)
+
+    def __eq__(self, other):
+        return self._cmp("lt", other)
+
+    def __ne__(self, other):
+        return self._cmp("lt", other)
+
+    def __gt__(self, other):
+        return self._cmp("lt", other)
+
+    def __ge__(self, other):
+        return self._cmp("lt", other)
+
+    def __repr__(self):
+        return "<comhyle V - %s>" % (self.version_string)
